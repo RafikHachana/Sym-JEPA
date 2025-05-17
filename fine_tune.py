948032aba3951be4feb9630857721e7eeed1f3e5
Rafik Hachana <rafikhachana@gmail.com>
2025-05-17 10:44:33 +0000

    add melody completion, and a fast dev run option

--- fine_tune.py
+++ fine_tune.py
@@ -1,4 +1,5 @@
 from tasks.genre_classification import GenreClassificationModel
+from tasks.melody_completion import MelodyCompletionModel
 from dataset import MidiDataModule
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
@@ -19,7 +20,10 @@ def main():
 
     parser.add_argument('--max_epochs', type=int, default=3)
     parser.add_argument('--task', type=str, default='genre',
-    choices=['genre', 'style'], help='Task to fine-tune on')
+    choices=['genre', 'style', 'melody_completion'], help='Task to fine-tune on')
+
+    parser.add_argument("--fast_dev_run", action="store_true",
+                       help="Do a test run with 1 batch for training and validation")
                       
     args = parser.parse_args()
 
@@ -31,36 +35,49 @@ def main():
         num_workers=4,
         skip_unknown_genres=args.task == 'genre',
         skip_unknown_styles=args.task == 'style',
-        tokenization=args.tokenization
+        tokenization=args.tokenization,
+        generate_melody_completion_pairs=args.task == 'melody_completion'
     )
 
     data_module.setup()
 
-    model = GenreClassificationModel(
-        num_classes=len(data_module.train_ds.all_genres) if args.task == 'genre' else len(data_module.train_ds.all_styles),
-        lr=1e-2,
-        d_model=512,
-        encoder_layers=8,
-        num_attention_heads=8,
+    if args.task == 'genre' or args.task == 'style':
+        model = GenreClassificationModel(
+            num_classes=len(data_module.train_ds.all_genres) if args.task == 'genre' else len(data_module.train_ds.all_styles),
+            lr=1e-2,
+            d_model=512,
+            encoder_layers=8,
+            num_attention_heads=8,
         tokenization=args.tokenization,
         class_weights=torch.tensor([1/(x+1e-7) for x in data_module.train_ds.genre_counts]) if args.task == 'genre' else torch.tensor([1/(x+1e-7) for x in data_module.train_ds.style_counts]),
         task=args.task
-    )
+        )
+    elif args.task == 'melody_completion':
+        model = MelodyCompletionModel(
+            lr=1e-2,
+            d_model=512,
+            encoder_layers=8,
+            tokenization=args.tokenization
+        )
 
     if args.model_path:
         model.load_jepa(args.model_path)
 
-    logger = WandbLogger(
-        project=f"symjepa-{args.task}-classification",
-        entity="rh-iu",
-    )
+    if args.fast_dev_run:
+        logger = None
+    else:
+        logger = WandbLogger(
+            project=f"symjepa-{args.task}-classification",
+            entity="rh-iu",
+        )
 
     trainer = pl.Trainer(
         max_epochs=args.max_epochs,
         callbacks=[
             ModelCheckpoint(monitor='val_loss', mode='min', save_top_k=1, save_last=True)
         ],
-        logger=logger
+        logger=logger,
+        fast_dev_run=args.fast_dev_run
     )
 
     trainer.validate(model, data_module)
