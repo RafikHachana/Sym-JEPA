a5d4428caac91073021e645ba272b02bc904eb54
Rafik Hachana <rafikhachana@gmail.com>
2025-05-20 19:11:08 +0000

    use a focal loss, use transformer with absolute pe, freeze embedding weights when training

--- tasks/genre_classification/model.py
+++ tasks/genre_classification/model.py
@@ -9,9 +9,23 @@ from model import SymJEPA
 import wandb
 from sklearn.metrics import roc_auc_score, confusion_matrix
 
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2):
+        super().__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        
+    def forward(self, inputs, targets, weight=None):
+        ce_loss = F.cross_entropy(inputs, targets, reduction='none', weight=weight)
+        pt = torch.exp(-ce_loss)
+        focal_loss = self.alpha * (1-pt)**self.gamma * ce_loss
+        return focal_loss.mean() * 1e10
+
 class GenreClassificationModel(pl.LightningModule):
     def __init__(self, 
                     num_classes,
+                    max_len=2048,
                     lr=1e-3,
                     d_model=512,
                     encoder_layers=8,
@@ -20,6 +34,7 @@ class GenreClassificationModel(pl.LightningModule):
                     tokenization='remi',
                     class_weights=None,
                     task='genre',
+                    use_focal_loss=True,
                  **kwargs):
         super().__init__()
 
@@ -27,25 +42,42 @@ class GenreClassificationModel(pl.LightningModule):
         vocab_size=1,
         pad_token_id=0,
         hidden_size=d_model,
-        num_hidden_layers=encoder_layers,
-        num_attention_heads=num_attention_heads,
-        intermediate_size=intermediate_size,
+        num_hidden_layers=2,
+        num_attention_heads=8,
+        intermediate_size=2048,
         max_position_embeddings=1024,
-        position_embedding_type='relative_key_query'
+        # position_embedding_type='relative_key_query'
         )
+
+        self.max_len = max_len
         
         # Initialize only the encoder
         self.jepa = SymJEPA(tokenization=tokenization, pass_target_mask_to_predictor=True)
 
-        self.genre_classifier = nn.Sequential(
-            nn.Linear(d_model, d_model),
-            nn.GELU(),
-            nn.Dropout(0.1),
-            nn.Linear(d_model, d_model // 2),
-            nn.GELU(), 
-            nn.Dropout(0.1),
-            nn.Linear(d_model // 2, num_classes)
+
+        # Add sinusoidal positional encoding
+        self.positional_encoding = nn.Parameter(torch.zeros(1, 256, d_model))
+        nn.init.xavier_uniform_(self.positional_encoding)
+
+        self.loss = FocalLoss() if use_focal_loss else nn.CrossEntropyLoss()
+
+
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=d_model,
+            nhead=8,
+            dim_feedforward=2048,
+            dropout=0.1,
+            activation='relu',
         )
+        # Transformer encoder with one layer
+        self.genre_transformer = nn.TransformerEncoder(
+            encoder_layer,
+            num_layers=1
+        )
+
+        # self.genre_transformer = BertModel(config=encoder_config)
+
+        self.genre_classifier = nn.Linear(d_model, num_classes)
 
         self.lr = lr
         self.d_model = d_model
@@ -59,10 +91,12 @@ class GenreClassificationModel(pl.LightningModule):
         self.class_key = 'genre_id' if task == 'genre' else 'style_id'
 
     def forward(self, input_ids):
-        encoder_hidden = self.jepa.encode_context(input_ids)
-        # Mean pool over the sequence length
-        encoder_hidden = torch.mean(encoder_hidden, dim=1)
-        logits = self.genre_classifier(encoder_hidden)
+        with torch.no_grad():
+            encoder_hidden = self.jepa.encode_context(input_ids)
+            encoder_hidden = encoder_hidden + self.positional_encoding
+        genre_hidden = self.genre_transformer(encoder_hidden)
+        genre_hidden = genre_hidden.mean(dim=1)
+        logits = self.genre_classifier(genre_hidden)
         return logits
 
     def training_step(self, batch, batch_idx):
@@ -70,7 +104,7 @@ class GenreClassificationModel(pl.LightningModule):
         # attention_mask = batch['attention_mask']
         # pdb.set_trace()
         logits = self(input_ids)
-        loss = F.cross_entropy(logits, batch[self.class_key],
+        loss = self.loss(logits, batch[self.class_key],
         weight=self.class_weights.to(self.device)
         )
         self.log('train_loss', loss)
@@ -79,7 +113,9 @@ class GenreClassificationModel(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         input_ids = batch['input_ids']
         logits = self(input_ids)
-        loss = F.cross_entropy(logits, batch[self.class_key])
+        loss = self.loss(logits, batch[self.class_key],
+        weight=self.class_weights.to(self.device)
+        )
 
         self.log('val_loss', loss)
         preds = torch.argmax(logits, dim=1)
