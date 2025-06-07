6bc5cd87c9d1afb1734432ed70585fc23264bf70
RafikHachana <rafikhachana@gmail.com>
2025-06-07 14:43:59 +0300

    masking improvements and addition of latent var id

--- masking.py
+++ masking.py
@@ -1,24 +1,36 @@
+import random
+from typing import Dict, Tuple
 from octuple_tokenizer import OctupleVocab
 import torch
 
-def random_masking(input_ids, octuple_breakout, mask_prob=0.2):
+
+transpose_range = 12
+instrument_range = 128
+
+def _get_transpose_id(semitones):
+    return (semitones + transpose_range) % transpose_range
+
+def _get_instrument_id(instrument):
+    return instrument + transpose_range
+
+def random_masking(input_ids, octuple_breakout, mask_prob=0.1):
     """
     Masks random tokens in the input sequence.
     """
-    context_mask = torch.bernoulli(torch.ones(input_ids.size()) * mask_prob)
+    context_mask = torch.bernoulli(torch.ones(input_ids.size()) * mask_prob).bool()
     target_mask = ~context_mask
 
     context_mask = context_mask.to(input_ids.device)
     target_mask = target_mask.to(input_ids.device)
 
-    return context_mask, target_mask
+    return context_mask, target_mask, input_ids, 0
 
 def contiguous_masking(input_ids, octuple_breakout, context_ratio=0.5):
     """
     Masks contiguous tokens in the input sequence.
     """
     # Random contiguous masking
-    seq_length = input_ids.size()
+    seq_length = input_ids.size()[0]
     mask_step = 8
     mask_start_max = int(seq_length // mask_step * context_ratio) * mask_step - mask_step
     assert mask_start_max % mask_step == 0, f"mask_start_max {mask_start_max} is not divisible by mask_step {mask_step}"
@@ -29,15 +41,15 @@ def contiguous_masking(input_ids, octuple_breakout, context_ratio=0.5):
     mask = torch.zeros(seq_length, dtype=torch.bool)
     mask[mask_start:mask_start+mask_size] = True
 
-    target_mask_start = torch.randint(1, (mask_start + mask_size) // mask_step, (1,)) * mask_step
-    target_mask_size = torch.randint(1, (mask_start + mask_size - target_mask_start) // mask_step, (1,)) * mask_step
-    target_mask = torch.zeros(seq_length, dtype=torch.bool)
-    target_mask[target_mask_start:target_mask_start+target_mask_size] = True
+    target_mask_start = torch.randint(mask_start.item() // mask_step, (mask_start.item() + mask_size) // mask_step, (1,)) * mask_step
+    target_mask_size = torch.randint(1, (mask_start.item() + mask_size - target_mask_start.item()) // mask_step, (1,)) * mask_step
+    target_mask = torch.ones(seq_length, dtype=torch.bool)
+    target_mask[target_mask_start:target_mask_start+target_mask_size] = False
 
-    return mask, target_mask
+    return mask, target_mask, input_ids, 0
 
 def instrument_masking(input_ids, octuple_breakout):
-    unique_instruments = set([x for x in octuple_breakout["instrument"] if x is not None])
+    unique_instruments = list(set([x for x in octuple_breakout["instrument"] if x is not None]))
 
     if len(unique_instruments) == 1:
         # Fallback to random masking
@@ -46,7 +58,7 @@ def instrument_masking(input_ids, octuple_breakout):
     # Pick a random instrument
     instrument = unique_instruments[torch.randint(0, len(unique_instruments), (1,))]
 
-    context_mask = torch.zeros_like(input_ids)
+    context_mask = torch.zeros_like(input_ids).bool()
 
     for i, x in enumerate(octuple_breakout['instrument']):
         if x is None:
@@ -56,11 +68,11 @@ def instrument_masking(input_ids, octuple_breakout):
     
     target_mask = ~context_mask
 
-    return context_mask, target_mask
+    return context_mask, target_mask, input_ids, _get_instrument_id(instrument)
 
 
 def transpose_masking(input_ids, octuple_breakout):
-    semitones = torch.randint(-5, 6, (1,))
+    semitones = torch.randint(-5, 7, (1,))
     
     octuple_breakout['pitch'] = [None if x is None else x + semitones if x <= 127 else None for x in octuple_breakout['pitch']]
 
@@ -72,8 +84,8 @@ def transpose_masking(input_ids, octuple_breakout):
 
         encoded = octuple_vocab.encode(f"<3-{x}>")
         input_ids[i*8+3] = encoded[0]
-
-    return input_ids
+    mask = torch.zeros_like(input_ids).bool()
+    return mask, mask, input_ids, _get_transpose_id(semitones.item())
 
 def rhythmic_noise_masking(input_ids, octuple_breakout):
 
@@ -86,16 +98,16 @@ def rhythmic_noise_masking(input_ids, octuple_breakout):
             continue
         if x < 0:
             x = 0
-        if x > 127:
-            x = 127
+        if x > 7:
+            x = 7
 
         encoded = octuple_vocab.encode(f"<4-{x}>")
         input_ids[i*8+4] = encoded[0]
-
-    return input_ids
+    mask = torch.zeros_like(input_ids).bool()
+    return mask, mask, input_ids, 0
 
 def mask_pitch_classes(input_ids, octuple_breakout):
-    pitch_classes = set([x % 12 for x in octuple_breakout['pitch'] if x is not None and x <= 127])
+    pitch_classes = list(set([x % 12 for x in octuple_breakout['pitch'] if x is not None and x <= 127]))
 
     if len(pitch_classes) == 1:
         # Fallback to random masking
@@ -104,7 +116,7 @@ def mask_pitch_classes(input_ids, octuple_breakout):
     # Pick a random pitch class
     pitch_class = pitch_classes[torch.randint(0, len(pitch_classes), (1,))]
 
-    context_mask = torch.zeros_like(input_ids)
+    context_mask = torch.zeros_like(input_ids).bool()
 
     for i, x in enumerate(octuple_breakout['pitch']):
         if x is None or x > 127:
@@ -114,10 +126,10 @@ def mask_pitch_classes(input_ids, octuple_breakout):
 
     target_mask = ~context_mask
 
-    return context_mask, target_mask
+    return context_mask, target_mask, input_ids, 0
 
 def mask_octaves(input_ids, octuple_breakout):
-    octaves = set([x // 12 for x in octuple_breakout['pitch'] if x is not None and x <= 127])
+    octaves = list(set([x // 12 for x in octuple_breakout['pitch'] if x is not None and x <= 127]))
 
     if len(octaves) == 1:
         # Fallback to random masking
@@ -126,7 +138,7 @@ def mask_octaves(input_ids, octuple_breakout):
     # Pick a random octave
     octave = octaves[torch.randint(0, len(octaves), (1,))]
 
-    context_mask = torch.zeros_like(input_ids)
+    context_mask = torch.zeros_like(input_ids).bool()
 
     for i, x in enumerate(octuple_breakout['pitch']):
         if x is None or x > 127:
@@ -136,8 +148,48 @@ def mask_octaves(input_ids, octuple_breakout):
 
     target_mask = ~context_mask
 
-    return context_mask, target_mask
+    return context_mask, target_mask, input_ids, 0
 
+
+class RandomMaskGenerator:
+    def __init__(self, probs: Dict[str, float]):
+        self.probs = probs
+
+        assert sum(self.probs.values()) == 1.0, f"Probabilities must sum to 1.0, got {sum(self.probs.values())}"
+
+        self.mask_functions = {
+            "random": random_masking,
+            "contiguous": contiguous_masking,
+            "instrument": instrument_masking,
+            "transpose": transpose_masking,
+            "rhythmic_noise": rhythmic_noise_masking,
+            "pitch_classes": mask_pitch_classes,
+            "octaves": mask_octaves,
+        }
+
+        assert all(mask_name in self.mask_functions for mask_name in self.probs), f"All mask names must be in {self.mask_functions}"
+
+    def __call__(self, input_ids, octuple_breakout, random_mask_prob=0.1, contiguous_context_ratio=0.5) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, int, str]:
+        mask_function = random.choices(list(self.probs.keys()), weights=list(self.probs.values()))[0]
+        print(f"Masking with {mask_function}")
+        if mask_function == "random":
+            result = self.mask_functions[mask_function](input_ids, octuple_breakout, mask_prob=random_mask_prob)
+        elif mask_function == "contiguous":
+            result = self.mask_functions[mask_function](input_ids, octuple_breakout, context_ratio=contiguous_context_ratio)
+        else:
+            result = self.mask_functions[mask_function](input_ids, octuple_breakout)
+        
+        return *result, mask_function
+
+if __name__ == "__main__":
+    # mask_generator = RandomMaskGenerator({"random": 0.1, "contiguous": 0.3, "instrument": 0.1, "transpose": 0.1, "rhythmic_noise": 0.2, "pitch_classes": 0.1, "octaves": 0.1})
+    mask_generator = RandomMaskGenerator({"transpose": 1.0})
+    input_ids = torch.randint(0, 128, (64,))
+    octuple_breakout = {"instrument": [1, 2, 1, 2, 1, 1, 1, 1], "pitch": [60, 75, 60, 70, 60, 58, 60, 61], "duration": [1, 2, 1, 5, 1, 1, 1, 1]}
+    print(mask_generator(input_ids, octuple_breakout))
+
+
+    
 
 
     
